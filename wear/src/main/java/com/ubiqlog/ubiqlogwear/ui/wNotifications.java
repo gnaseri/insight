f82a9707db76d2bf838833dfcb0c93b79d673e3f
Manouchehr <manouriz@websriver.com>
2015-02-04 12:32:06 +0100

    UI Updated: Bluetooth + Notifications
    Rewriting the list's adapter class to provide an ability to add buttons
    below the data list
    
    new classes:
    RowData: a datatype to store logs data
    
    changed classes:
    MyListAdapter -> MultipleListItemsAdapter
    activity_barlist -> activity_datalist
    activity_barlist_row -> activity_datalist_row

--- wear/src/main/java/com/ubiqlog/ubiqlogwear/ui/wNotifications.java
+++ wear/src/main/java/com/ubiqlog/ubiqlogwear/ui/wNotifications.java
@@ -2,36 +2,78 @@ package com.ubiqlog.ubiqlogwear.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.ubiqlog.ubiqlogwear.R;
-import com.ubiqlog.ubiqlogwear.ui.dummy.MyListAdapter;
+import com.ubiqlog.ubiqlogwear.utils.MultipleListItemsAdapter;
+import com.ubiqlog.ubiqlogwear.utils.RowData;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 
 public class wNotifications extends Activity {
+    private MultipleListItemsAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_barlist);
+        setContentView(R.layout.activity_datalist);
 
-        //set Titlebar Title
+        //set Title of activity
         TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvTitle.setText(R.string.title_activity_wnotifications);
 
-        //set Titlebar Logo
-        ImageView tvTitleLogo = (ImageView) findViewById(R.id.ivTitleLogo);
-        tvTitleLogo.setImageResource(R.drawable.notificn2);
-
-        //test data
-        String data[] = new String[]{"Facebook", "Email", "Battery", "Email", "Email", "Viber"};
-
-        //map data to layout
-        ListView list = (ListView) findViewById(R.id.listView);
-        MyListAdapter adapter = new MyListAdapter(this, data);
-        list.setAdapter(adapter);
-
+        displayDataList(new Date());
     }
 
+    public void displayDataList(Date date) {
+        mAdapter = new MultipleListItemsAdapter(this);
+
+        //generate sample data
+        for (RowData rData : generateTestData(date)) {
+            mAdapter.addItem(rData);
+        }
+
+        //generate sample dates(past three days) for links buttons
+        for (int i = 0; i < 3; i++)
+            mAdapter.addLinkButtonItem(new SimpleDateFormat("MM/dd/yyyy").format(new Date(date.getTime() - (i + 1) * 24 * 60 * 60 * 1000)));
+
+        //set adapter to listView
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(mAdapter);
+
+        // list item's 'clicable' property on activity is disabled. User can only click on 'LinkButtons'
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String selectedDate = view.findViewById(R.id.tvLinkText).getTag().toString();
+                try {
+                    Date date = new SimpleDateFormat("MM/dd/yyyy").parse(selectedDate);
+                    displayDataList(date);
+
+                } catch (ParseException e) {
+                }
+            }
+        });
+    }
+
+    public List<RowData> generateTestData(Date time) {
+        List<RowData> rDatas = new ArrayList<RowData>();
+        String status[] = {"Facebook", "Viber", "Battery", "Email", "Unknown"};
+        for (int j = 0; j < 10; j++) {
+            Random randomGenerator = new Random();
+            RowData rData = new RowData(new Date(time.getTime() + j * 3 * 60 * 1000), status[randomGenerator.nextInt(3)], R.drawable.baricons_bullet);
+            rDatas.add(rData);
+        }
+        return rDatas;
+
+    }
 }
---
+++ wear/src/main/java/com/ubiqlog/ubiqlogwear/utils/MultipleListItemsAdapter.java
@@ -0,0 +1,144 @@
+package com.ubiqlog.ubiqlogwear.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ubiqlog.ubiqlogwear.R;
+
+/**
+ * Created by Manouchehr on 2/4/2015.
+ */
+public class MultipleListItemsAdapter extends BaseAdapter {
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_LinkButton = 1;
+    private static final int TYPE_MAX_COUNT = 2;
+
+    private ArrayList mData = new ArrayList();
+    private LayoutInflater mInflater;
+    private Activity activity;
+    private TreeSet mLinkButtonsSet = new TreeSet();
+
+    public MultipleListItemsAdapter(Activity activity) {
+        this.activity = activity;
+        mInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItem(final RowData item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void addLinkButtonItem(final String item) {
+        mData.add(item);
+        // save mLinkButtonsSet position
+        mLinkButtonsSet.add(mData.size() - 1);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mLinkButtonsSet.contains(position) ? TYPE_LinkButton : TYPE_ITEM;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return TYPE_MAX_COUNT;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        int type = getItemViewType(position);
+        if (convertView == null) {
+            //cache views for performance
+            holder = new ViewHolder();
+            switch (type) {
+                case TYPE_ITEM:
+                    convertView = mInflater.inflate(R.layout.activity_datalist_row, null);
+                    holder.item_StatusImage = (ImageView) convertView.findViewById(R.id.ivStatus);
+                    holder.item_Time = (TextView) convertView.findViewById(R.id.tvTime);
+                    holder.item_Date = (TextView) convertView.findViewById(R.id.tvDate);
+                    holder.item_Title = (TextView) convertView.findViewById(R.id.tvTitle);
+                    break;
+
+                case TYPE_LinkButton:
+                    convertView = mInflater.inflate(R.layout.activity_linkslist_row, null);
+                    holder.link_Text = (TextView) convertView.findViewById(R.id.tvLinkText);
+                    break;
+            }
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        //map data to views
+        switch (type) {
+            case TYPE_ITEM:
+                RowData rData = (RowData) mData.get(position);
+                holder.item_Title.setText(rData.statusText);
+                holder.item_Date.setText(new SimpleDateFormat("MM/dd/yyyy").format(rData.time));
+                holder.item_Time.setText(new SimpleDateFormat("hh:mm").format(rData.time));
+                holder.item_StatusImage.setBackground(activity.getResources().getDrawable(getBackgroundImage(getCount(), position)));
+                holder.item_StatusImage.setPadding(6, 6, 6, 6);
+                if (Integer.valueOf(rData.icon).equals(null))
+                    holder.item_StatusImage.setImageResource(R.drawable.baricons_bullet);
+                else
+                    holder.item_StatusImage.setImageResource(rData.icon);
+                break;
+
+            case TYPE_LinkButton:
+                String linkText = (String) mData.get(position);
+                holder.link_Text.setText(linkText);
+                holder.link_Text.setTag(linkText);//set tag property to discover which button clicked on activity side
+                break;
+        }
+        return convertView;
+    }
+
+    public static class ViewHolder {
+        // Data Items views
+        public TextView item_Time;
+        public TextView item_Date;
+        public TextView item_Title;
+        public ImageView item_StatusImage;
+
+        //LinkButton views
+        public TextView link_Text;
+    }
+
+    public int getBackgroundImage(int count, int position) {
+        if (position == 0)
+            return R.drawable.baricons_start;
+        else if (position == count - mLinkButtonsSet.size() - 1)
+            return R.drawable.baricons_end;
+        else
+            return R.drawable.baricons_mid;
+    }
+}
+
