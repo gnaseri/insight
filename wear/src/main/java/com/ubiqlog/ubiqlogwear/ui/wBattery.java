8a2e7e285e146639244142fbb1b2363ae9968321
Manouchehr <manouriz@websriver.com>
2015-03-04 14:50:56 +0100

    UI merged from developer branch
    - because of an error I couldn't merge developer with master branch and
    unfortunately I had to delete it.
    - I recovered changed files from my own backups
    - summary of changes:
    - added an animation indicator for scrollbar
    - AppUsage: Labels alignment changed
    - Labels Colors changed to gray(chart_labels_color)
    - Added "Last Synced" label

--- wear/src/main/java/com/ubiqlog/ubiqlogwear/ui/wBattery.java
+++ wear/src/main/java/com/ubiqlog/ubiqlogwear/ui/wBattery.java
@@ -1,22 +1,27 @@
 package com.ubiqlog.ubiqlogwear.ui;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.ubiqlog.ubiqlogwear.R;
-import com.ubiqlog.ubiqlogwear.sensors.BatterySensor;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
@@ -40,8 +45,6 @@ public class wBattery extends Activity {
         setContentView(R.layout.fragment_chart);
 
 
-
-
         //set Title of activity
         TextView tvTitle = (TextView) findViewById(R.id.tvTitleChart);
         tvTitle.setText(R.string.title_activity_wbattery);
@@ -52,20 +55,62 @@ public class wBattery extends Activity {
     }
 
     private void displayData(Date date) {
+        final TextView tvDate = (TextView) findViewById(R.id.tvDate);
+        tvDate.setText(new SimpleDateFormat("MM/dd/yyyy").format(date));
+
+        final TextView tvLastSync = (TextView) findViewById(R.id.tvLastSync);
+        tvLastSync.setText("Last Sync: " + new SimpleDateFormat("MM/dd/yyyy hh:mm").format(date));
+
         final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
         final LinearLayout frameBox = (LinearLayout) findViewById(R.id.frameBox);
 
-        // remove all added views before except linksbox
-        frameBox.removeViewsInLayout(0, frameBox.getChildCount() - 1);
+        // remove all added views before except linksbox and tvLastSync label
+        frameBox.removeViewsInLayout(1, frameBox.getChildCount() - 2);
 
         FrameLayout chart = new FrameLayout(this);
         LinearLayout.LayoutParams cParams = new LinearLayout.LayoutParams(getSizeInDP(190), getSizeInDP(170));
         cParams.gravity = (Gravity.TOP | Gravity.CENTER_HORIZONTAL);
         chart.setLayoutParams(cParams);
-        chart.setPadding(3, 10, 10, 10);
+        chart.setPadding(10, 0, 15, 10);
         chart.addView(createGraph(date));
-        frameBox.addView(chart,0);
+        frameBox.addView(chart, 1);
 
+
+        // add a cursor point to show the user the scroll feature ////////////////////////////////////////////////////////
+        final AnimationSet aniSetCursor = new AnimationSet(true);
+        final AlphaAnimation aniAlpha = new AlphaAnimation(1.0f, 0.0f);
+        aniAlpha.setDuration(1500);
+        aniAlpha.setRepeatCount(2);
+        aniAlpha.setFillAfter(true);
+        aniSetCursor.addAnimation(aniAlpha);
+
+        TranslateAnimation aniMove = new TranslateAnimation(0.0f, 0.0f, -10.0f, 20.0f);          //  TranslateAnimation(xFrom, xTo, yFrom, yTo)
+        aniMove.setDuration(1500);
+        aniMove.setRepeatCount(2);
+        aniSetCursor.addAnimation(aniMove);
+
+        final ImageView linksCursor = (ImageView) findViewById(R.id.linksCursor);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics); // get screen properties ex. size
+        RelativeLayout.LayoutParams linksCursorParams = (RelativeLayout.LayoutParams) linksCursor.getLayoutParams();
+        linksCursorParams.setMargins(0, displayMetrics.heightPixels - 35, 0, 0); // set position of cursor in bottom of screen
+        linksCursor.setTag(null);
+        linksCursor.startAnimation(aniSetCursor);
+
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                int scrollY = scrollView.getScrollY();
+                if (scrollY > 10 && linksCursor.getTag() == null) {
+                    aniAlpha.setRepeatCount(0);
+                    linksCursor.startAnimation(aniAlpha);
+                    linksCursor.setTag("displayed");
+                }
+            }
+        });
+
+
+        //render Links box //////////////////////////////////////////////////////////////////////////////////////////
         final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -141,6 +186,7 @@ public class wBattery extends Activity {
         mRenderer.setPanEnabled(false, false);
         mRenderer.setShowGrid(false);
         mRenderer.setBackgroundColor(Color.WHITE);
+        mRenderer.setMargins(new int[]{10, 20, 5, 20});  //setMargins(top, left, bottom, right) defaults(20,30,10,20) ** more space in left and right for labels
         mRenderer.setMarginsColor(Color.WHITE);
         mRenderer.setAxesColor(Color.BLACK);
         mRenderer.setApplyBackgroundColor(true);
@@ -149,9 +195,13 @@ public class wBattery extends Activity {
         mRenderer.setXLabelsColor(getResources().getColor(R.color.chart_labels_color));
         mRenderer.setYLabelsColor(0, getResources().getColor(R.color.chart_labels_color));
 
-        mRenderer.setChartTitle(new SimpleDateFormat("MM/dd/yyyy").format(date));
+        //mRenderer.setChartTitle(new SimpleDateFormat("MM/dd/yyyy").format(date));
+
         GraphicalView chartView = ChartFactory.getLineChartView(this, dataset, mRenderer);
         return chartView;
     }
-    private int getSizeInDP(int x){return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, x, getResources().getDisplayMetrics());}
+
+    private int getSizeInDP(int x) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, x, getResources().getDisplayMetrics());
+    }
 }
