3da3a0f8d324757ec38157276f792121bebc4a87
Reza <rrawasi@gmail.com>
2015-04-13 10:09:29 -0700

    Update Setting.java

--- wear/src/main/java/com/insight/insight/common/Setting.java
+++ wear/src/main/java/com/insight/insight/common/Setting.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
     Created by CM
  */
 
+//TODO 4m Reza: static file names should be converted to CAPITAL letters
 public final class Setting {
 
     public static final long SAVE_FILE_WAIT_INTERVAL = 10000L; // 10 seconds
@@ -15,7 +16,7 @@ public final class Setting {
     public static final int bufferMaxSize = 10; // default: 10
 
     public static final int linksButtonCount = 7;
-
+    
     public static final String dataFilename_Battery ="BatterySensor";
     public static final String dataFilename_Bluetooth ="Bluetooth";
     public static final String dataFilename_Notifications ="Notif";
