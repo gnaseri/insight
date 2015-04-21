ccd007f519b2652a43445e99bfc18ea502773c35
Manouchehr <manouriz@websriver.com>
2015-04-21 20:50:38 +0200

    UI - Light Sensor sampling method modified

--- wear/src/main/java/com/insight/insight/sensors/LightSensor.java
+++ wear/src/main/java/com/insight/insight/sensors/LightSensor.java
@@ -23,8 +23,8 @@ import java.util.Date;
  */
 
 /**
- *  Class will take 5 lux readings every SensorConstant.LIGHT_SENSOR_INTERVAL
-    and write to file
+ * Class will take 5 lux readings every SensorConstant.LIGHT_SENSOR_INTERVAL
+ * and write to file
  */
 
 public class LightSensor extends Service implements SensorEventListener {
@@ -33,8 +33,7 @@ public class LightSensor extends Service implements SensorEventListener {
     private SensorManager mSensorManager;
     private Handler mHandler;
     int count;
-    float avg;
-
+    float totalSum;
 
     private DataAcquisitor mDataBuffer;
     private DataAcquisitor mSA_lightBuffer;
@@ -50,8 +49,9 @@ public class LightSensor extends Service implements SensorEventListener {
     @Override
     public void onCreate() {
         count = 0;
-        mDataBuffer = new DataAcquisitor(this,this.getClass().getSimpleName());
-        mSA_lightBuffer = new DataAcquisitor(this, "SA/LightSensor");
+        totalSum = 0f;
+        mDataBuffer = new DataAcquisitor(this, Setting.dataFilename_LightSensor);
+        mSA_lightBuffer = new DataAcquisitor(this, "SA/" + Setting.dataFilename_LightSensor);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         Log.d(LOG_TAG, "Light sensor started");
@@ -59,9 +59,9 @@ public class LightSensor extends Service implements SensorEventListener {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent != null){
+        if (intent != null) {
             //SensorDelayNormal is 200,000 ms
-            HandlerThread ht = new HandlerThread("LightThread",android.os.Process.THREAD_PRIORITY_BACKGROUND);
+            HandlerThread ht = new HandlerThread("LightThread", android.os.Process.THREAD_PRIORITY_BACKGROUND);
             ht.start();
             mHandler = new Handler(ht.getLooper());
             mHandler.post(activateLightListener);
@@ -76,35 +76,32 @@ public class LightSensor extends Service implements SensorEventListener {
 
         //After taking 3 readings, unregister the listener
         //Call handler again in SensorConstant.Interval
-        if (count > SensorConstants.LIGHT_SAMPLE_AMNT){
+        if (count >= SensorConstants.LIGHT_SAMPLE_AMNT) {
             mSensorManager.unregisterListener(this);
-            count = 0;
 
             Date date = new Date();
-
+            float avg = totalSum / count;
             //Encode the lux value and date
             String encoded = JSONUtil.encodeLight(avg, date);
             Log.d(LOG_TAG, encoded);
 
-
             //add encoded string to buffer
-            mDataBuffer.insert(encoded,true, Setting.bufferMaxSize);
+            mDataBuffer.insert(encoded, true, Setting.bufferMaxSize);
             mDataBuffer.flush(true);
 
             String encoded_SA = SemanticTempCSVUtil.encodeLight(avg, date);
-            mSA_lightBuffer.insert(encoded_SA,true,Setting.bufferMaxSize);
+            mSA_lightBuffer.insert(encoded_SA, true, Setting.bufferMaxSize);
             mSA_lightBuffer.flush(true);
-            avg = 0;
+            totalSum = 0;
+            count = 0;
 
-            mHandler.postDelayed(activateLightListener,SensorConstants.LIGHT_SENSOR_INTERVAL);
+            mHandler.postDelayed(activateLightListener, SensorConstants.LIGHT_SENSOR_INTERVAL);
+
+        } else {
+            float lux = event.values[0];
+            totalSum += lux;
+            count++;
         }
-
-        float lux = event.values[0];
-        avg = (avg + lux) / 2f;
-        count++;
-
-
-
     }
 
     @Override
@@ -124,7 +121,7 @@ public class LightSensor extends Service implements SensorEventListener {
     private Runnable activateLightListener = new Runnable() {
         @Override
         public void run() {
-            mSensorManager.registerListener(LightSensor.this,mLight,SensorManager.SENSOR_DELAY_FASTEST);
+            mSensorManager.registerListener(LightSensor.this, mLight, SensorManager.SENSOR_DELAY_FASTEST);
         }
     };
 }
